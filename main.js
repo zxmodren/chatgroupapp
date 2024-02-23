dc512edd326bcda766ea9729dc5d365449af113e
arya <aryaferdyansahxiii@gmail.com>
2024-02-23 18:06:04 +0700

    add new

--- main.js
+++ main.js
@@ -9,82 +9,81 @@ const dockIcon = path.join(__dirname, 'assets', 'img', 'chat_logo.png');
 const trayIcon = path.join(__dirname, 'assets', 'img', 'chat_tray_icon.png');
 
 
-function createSplashdWindow() {
-    // Browser Window <- Renderer Process
-    const win = new BrowserWindow({
-        icon: dockIcon,
-        width: 400,
-        height: 200,
-        backgroundColor: '#6e707e',
-        frame: false,
-        transparent: true,
-        webPreferences: {
-            nodeIntegration: false,
-            worldSafeExecuteJavaScript: true,
-            contextIsolation: true,
-        }
-    })
-
-    win.loadFile('second.html')
-    return win;
+function createSplashWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 200,
+    frame: false,
+    transparent: true,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    }
+  })
+  win.loadFile('splash.html')
+  return win;
 }
-
 function createWindow() {
-    const win = new BrowserWindow({
-        icon: dockIcon,
-        width: 1200,
-        height: 800,
-        show: false,
-        webPreferences: {
-            nodeIntegration: false,
-            worldSafeExecuteJavaScript: true,
-            contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.loadFile('index.html')
-    isDev && win.webContents.openDevTools();
-    return win;
+  const win = new BrowserWindow({
+    height: 550,
+    backgroundColor: '#6e707e',
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+  isDev && win.webContents.openDevTools();
+  return win;
 }
 if (isDev) {
-    require('electron-reload')(__dirname, {
-        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
-    })
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
 }
-
-// if (process.platform === 'win32') {
-//     app.dock.setIcon(dockIcon);
-// }
-
+if (process.platform === 'darwin') {
+  app.dock.setIcon(dockIcon);
+}
 let tray = null;
 app.whenReady()
-.then(() => {
-    const template = require('./src/js/utils/Menu').createTemplate(app);
-    const menu = Menu.buildFromTemplate(template);
-    const splash = createSplashdWindow();
-    const mainApp = createWindow();
-
-    Menu.setApplicationMenu(menu);
-    tray = new Tray(trayIcon);
-    tray.setContextMenu(menu);
-
-    mainApp.once('ready-to-show', () => {
-        // splash.destroy();
-        // mainApp.show();
-        
-        setTimeout(() => {
-            splash.destroy();
-            mainApp.show();
-        }, 2000)
+    .then(() => {
+        const template = require('./src/js/utils/Menu').createTemplate(app);
+        const menu = Menu.buildFromTemplate(template);
+        Menu.setApplicationMenu(menu);
+        tray = new Tray(trayIcon);
+        tray.setContextMenu(menu);
+        const splash = createSplashWindow();
+        const mainApp = createWindow();
+        mainApp.once('ready-to-show', () => {
+            // splash.destroy();
+            // mainApp.show();
+            setTimeout(() => {
+                splash.destroy();
+                mainApp.show();
+            }, 2000)
+        })
     });
-});
+
 
 ipcMain.on('notify', (_, message) => {
-    new Notification({ title: 'Notification', body: message }).show();
+  new Notification({title: 'Notification', body: message}).show();
 })
-
+ipcMain.on('app-quit', () => {
+  app.quit();
+})
 app.on('window-all-closed', () => {
-    app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 })
 
   
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     mode: 'development',
@@ -36,6 +37,7 @@ module.exports = {
             }
         ]
     },
+    plugins: [new Dotenv()],
     resolve: {
         extensions: ['.js'],
     },
